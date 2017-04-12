00806ad668c149304ede8e75f12e1c4edaa0e271
r.wobben <r.wobben@outlook.com>
2017-04-12 15:33:20 +0200

    made it work with a for-each

--- painting/models/repositories/IArtServiceAdapter.cs
+++ painting/models/repositories/IArtServiceAdapter.cs
@@ -7,7 +7,7 @@ namespace painting.models.repositories
     interface IArtServiceAdapter
     {
         Task<IEnumerable<string>> GetObjectNumberAsync(string key);
-        Task<string[]> GetDataPaintingsAsync(IEnumerable<string> numbers, string key);
+        Task<IEnumerable<painting.ViewModel.PaintingViewModel>> GetDataPaintingsAsync(IEnumerable<string> numbers, string key); 
     }
 }
 
