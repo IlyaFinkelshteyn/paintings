3a843540e73584886d4a20d7a3ccfc41c7c529c2
Pure Krome <github@world-domination.com.au>
2017-04-17 22:31:00 +1000

    Moved and cleaned up the Website code.

---
+++ src/Painting.Website/Repositories/IArtServiceAdapter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Painting.Website.Repositories
+{
+    public interface IArtServiceAdapter
+    {
+        Task<IEnumerable<string>> GetObjectNumberAsync(string key);
+        Task<IEnumerable<Painting.Website.ViewModel.PaintingViewModel>> GetDataPaintingsAsync(IEnumerable<string> numbers, string key); 
+    }
+}
+
+
+
---
+++ src/Painting.Website/ViewModel/PaintingViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Painting.Website.Models;
+
+namespace Painting.Website.ViewModel
+{
+    public class PaintingViewModel
+    {
+        public PaintingViewModel(string name, 
+            string title,
+            string description,
+            int year,
+            string collection,
+            string[] colors,
+            Image.Tile[] image)
+        {
+            Title = title;
+            Description = description;
+            Year = year;
+            Collection = collection;
+            Colors = colors;
+            Image = image;
+            Name = name; 
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int Year { get; private set; }
+        public string Collection { get; private set; }
+        public string Name { get; private set; }
+        public string[] Colors { get; private set; }
+        public Image.Tile[] Image { get; private set; }
+         
+    }
+
+}
